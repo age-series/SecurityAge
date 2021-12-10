274c41eb8b846ba99efcd67768c445cb92687c41
BomberPlayz <49914603+BomberPlayz@users.noreply.github.com>
2021-12-10 09:47:54 +0100

    Add files via upload

--- src/main/kotlin/org/ageseries/securityage/SecurityAge.kt
+++ src/main/kotlin/org/ageseries/securityage/SecurityAge.kt
@@ -1,9 +1,12 @@
 package org.ageseries.securityage
 
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.Item
 import org.ageseries.securityage.blocks.TestBlock
 import net.minecraft.world.level.block.Block
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
+import org.ageseries.securityage.blocks.LaserBlock
 import org.apache.logging.log4j.LogManager
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.forge.addGenericListener
@@ -15,6 +18,7 @@ object SecurityAge {
 
     init {
         MOD_BUS.addGenericListener({ event: RegistryEvent.Register<Block> -> registerBlocks(event) })
+        MOD_BUS.addGenericListener({ event: RegistryEvent.Register<Item> -> registerItems(event) })
         MOD_BUS.register(this)
     }
 
@@ -23,8 +27,27 @@ object SecurityAge {
         val testBlock = TestBlock()
         testBlock.setRegistryName(MODID, "test_block")
         event.registry.register(testBlock)
+        val laserBlock = LaserBlock()
+        laserBlock.setRegistryName(MODID, "laser_block")
+        event.registry.register(laserBlock)
 
         event.registry.keys.forEach {
+
+            LOGGER.info("${it.namespace}, ${it.path}")
+        }
+    }
+
+    private fun registerItems(event: RegistryEvent.Register<Item>) {
+        LOGGER.info("Registering items for Security Age")
+        var testBlockItem = BlockItem(TestBlock(), Item.Properties())
+        testBlockItem.setRegistryName(MODID, "test_block")
+        event.registry.register(testBlockItem)
+        var laserBlockItem = BlockItem(LaserBlock(), Item.Properties())
+        laserBlockItem.setRegistryName(MODID, "laser_block")
+        event.registry.register(laserBlockItem)
+
+        event.registry.keys.forEach {
+
             LOGGER.info("${it.namespace}, ${it.path}")
         }
     }
